669286fa9f35a5a7f6910ef3f766c0930e33a16b
studytimee <studytimee@gmail.com>
2014-03-26 00:43:47 +0000

    This example showing multiple pointers, pointing towords x varibale value. x variable hold value but pointer hold address of of variable inside memory.

---
+++ Pointer/fourPointers.c
@@ -0,0 +1,28 @@
+/*
+ * Pointer.c
+ *
+ *  Created on: 25 Mar 2014
+ *      Author: ali
+ */
+/*This example showing multiple pointers,
+pointing towords x varibale value. x variable
+hold value but pointer hold address of
+of variable inside memory.*/
+int main(){
+	int x, *p, **pp, ***ppp, ****pppp;
+	x = 10;
+	printf("%d\n", x);
+	p = &x;
+	*p = 20;
+	pp = &p;
+	**pp = 30;
+	ppp = &pp;
+	***ppp = 40;
+	pppp = &ppp;
+	****pppp = 50;
+	printf("%d", x);
+	return 0;
+}
+
+
+
